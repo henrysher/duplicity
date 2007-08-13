03042aa5b4987dc705b5c7acb3fb5eb4eb56b588
loafman <Unknown>
2007-08-13 18:16:41 +0000

    Upgrade to GPL version 3 license.

--- duplicity/librsync.py
+++ duplicity/librsync.py
@@ -4,7 +4,7 @@
 #
 # duplicity is free software; you can redistribute it and/or modify
 # under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # duplicity is distributed in the hope that it will be useful, but
