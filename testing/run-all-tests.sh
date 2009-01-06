6685098a11bcad446c9555bf90ad8d19685548e5
loafman <Unknown>
2009-01-06 17:32:08 +0000

    Added loop to run-all-tests.sh to run all tests against all supported versions of Python if available.  Looks for 2.3, 2.4, 2.5, 2.6.

--- testing/run-all-tests.sh
+++ testing/run-all-tests.sh
@@ -33,11 +33,20 @@ pwd
 
 ${SUDO} tar xzf testfiles.tar.gz
 
-for t in ${alltests}; do
-    echo "========== Running $t =========="
-    ${SUDO} python -u $t 2>&1 | grep -v "unsafe ownership"
-    echo
-    echo
+for v in 2.3 2.4 2.5 2.6; do
+    if [ -e /usr/bin/python$v ]; then
+        LOG=run-all-tests-$v.log
+        rm -f $LOG
+        echo "Running tests for python$v"
+        echo "Running tests for python$v" >> $LOG
+        for t in ${alltests}; do
+            echo "========== Running $t =========="
+            echo "========== Running $t ==========" >> $LOG
+            ${SUDO} python$v -u $t 2>&1 | grep -v "unsafe ownership" >> $LOG
+            echo >> $LOG
+            echo >> $LOG
+        done
+    fi
 done
 
 ${SUDO} rm -rf testfiles tempdir temp2.tar
