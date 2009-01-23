7ea0a3ed30e82bab664343ac2e46ca096789cc3b
loafman <Unknown>
2009-01-23 21:05:18 +0000

    Remove LOG entries.  Not needed.

--- testing/run-a-test.sh
+++ testing/run-a-test.sh
@@ -18,15 +18,9 @@ ${SUDO} tar xzf testfiles.tar.gz
 
 for v in 2.3 2.4 2.5 2.6; do
     if [ -e /usr/bin/python$v ]; then
-        LOG=run-all-tests-$v.log
-        rm -f $LOG
         echo "Running tests for python$v"
-        echo "Running tests for python$v" >> $LOG
         echo "========== Running $1 =========="
-        echo "========== Running $1 ==========" >> $LOG
-        ${SUDO} python$v -u $1 2>&1 | grep -v "unsafe ownership" >> $LOG
-        echo >> $LOG
-        echo >> $LOG
+        ${SUDO} python$v -u $1 2>&1 | grep -v "unsafe ownership"
     fi
 done
 
