ac716aa8fdbafb936ae067351e3249ba65d262bb
loafman <Unknown>
2009-01-24 22:16:17 +0000

    Move alltests list to separate file.

--- testing/run-all-tests.sh
+++ testing/run-all-tests.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-alltests=" \
-    backendtest.py \
-    collectionstest.py \
-    diffdirtest.py \
-    dup_temptest.py \
-    dup_timetest.py \
-    file_namingtest.py \
-    gpgtest.py \
-    gpgtest2.py \
-    GnuPGInterfacetest.py \
-    lazytest.py \
-    logtest.py \
-    manifesttest.py \
-    misctest.py \
-    patchdirtest.py \
-    pathtest.py \
-    rdiffdirtest.py \
-    roottest.py \
-    selectiontest.py \
-    statictest.py \
-    statisticstest.py \
-    tempdirtest.py \
-    test_tarfile.py \
-    finaltest.py \
-"
-
 SUDO=sudo
 
 cd `dirname $0`
@@ -41,14 +15,12 @@ for v in 2.3 2.4 2.5 2.6; do
     if [ -e /usr/bin/python$v ]; then
         LOG=run-all-tests-$v.log
         rm -f $LOG
-        echo "Running tests for python$v"
-        echo "Running tests for python$v" >> $LOG
-        for t in ${alltests}; do
-            echo "========== Running $t =========="
-            echo "========== Running $t ==========" >> $LOG
-            ${SUDO} python$v -u $t -v 2>&1 | grep -v "unsafe ownership" >> $LOG
-            echo >> $LOG
-            echo >> $LOG
+        echo "Running tests for python$v" | tee -a $LOG
+        for t in `cat alltests`; do
+            echo "========== Running $t ==========" | tee -a $LOG
+            ${SUDO} python$v -u $t -v 2>&1 | grep -v "unsafe ownership" | tee -a $LOG
+            echo | tee -a  $LOG
+            echo | tee -a  $LOG
         done
     fi
 done
--- testing/run-coverage.sh
+++ testing/run-coverage.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-alltests=" \
-    backendtest.py \
-    collectionstest.py \
-    diffdirtest.py \
-    dup_temptest.py \
-    dup_timetest.py \
-    file_namingtest.py \
-    gpgtest.py \
-    gpgtest2.py \
-    GnuPGInterfacetest.py \
-    lazytest.py \
-    logtest.py \
-    manifesttest.py \
-    misctest.py \
-    patchdirtest.py \
-    pathtest.py \
-    rdiffdirtest.py \
-    roottest.py \
-    selectiontest.py \
-    statictest.py \
-    statisticstest.py \
-    tempdirtest.py \
-    test_tarfile.py \
-    finaltest.py \
-"
-
 SUDO=sudo
 
 cd `dirname $0`
@@ -37,14 +11,19 @@ cd -
 
 ${SUDO} tar xzf testfiles.tar.gz
 
-ARGS='-u -m trace --count --file=counts --ignore-dir=/usr/lib/python2.5'
+ARGS=`python <<EOF
+import sys
+print "python -u -m trace --count --file=counts "
+for p in sys.path:
+    print "--ignore-dir=%s " % (p)
+EOF`
 
 ${SUDO} rm -rf counts coverage
 touch counts
 mkdir coverage
-for t in ${alltests}; do
-    CMD="${SUDO} python ${ARGS} $t"
-    echo "========== Running ${CMD} =========="
+for t in `cat alltests`; do
+    CMD="${SUDO} ${ARGS} $t -v"
+    echo "========== Running $t =========="
     ${CMD} 2>&1 | grep -v "unsafe ownership"
 done
 
@@ -55,7 +34,7 @@ python -m trace --report --missing --file=counts --coverdir=coverage
 
 # This bit of renaming exposes the oddly named .cover files,
 # at least on my system under Python 2.5.  If you're missing
-# files, just do an 'ls -la coverage' and they appear.
+# files, just do an 'ls -la coverage'.
 cd coverage
 rename 's/\.\.\.//g' ...*
 rename 's/.home.ken.workspace.duplicity-src.//g' .home*
