707b4964151941a8c6d41824cf284387a7135bfb
Michael Terry <michael.terry@canonical.com>
2009-05-30 15:18:40 +0200

    add info codes for upload events

---
+++ duplicity/backends/giobackend.py
@@ -0,0 +1,77 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2009 Michael Terry <mike@mterry.name>
+#
+# This file is part of duplicity.
+#
+# Duplicity is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# Duplicity is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with duplicity; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+import os
+import types
+
+import duplicity.backend
+from duplicity import log
+from duplicity import path
+from duplicity.errors import *
+
+import gio
+
+class GIOBackend(duplicity.backend.Backend):
+    """Use this backend when saving to a GIO URL
+    
+    Urls look like schema://server/path.
+    
+    """
+    def __init__(self, parsed_url):
+        duplicity.backend.Backend.__init__(self, parsed_url)
+        self.file = gio.File(parsed_url.url_string)
+
+    def put(self, source_path, remote_filename = None, rename = None):
+        """If rename is set, try that first, copying if doesn't work"""
+        if not remote_filename:
+            remote_filename = source_path.get_filename()
+        target_path = self.remote_pathdir.append(remote_filename)
+        log.Info("Writing %s" % target_path.name)
+        if rename:
+            try:
+                source_path.rename(target_path)
+            except OSError:
+                pass
+            else:
+                return
+        target_path.writefileobj(source_path.open("rb"))
+
+    def get(self, filename, local_path):
+        """Get file and put in local_path (Path object)"""
+        source_path = self.remote_pathdir.append(filename)
+        local_path.writefileobj(source_path.open("rb"))
+
+    def list(self):
+        """List files in that directory"""
+        try:
+                os.makedirs(self.remote_pathdir.base)
+        except:
+                pass
+        return self.remote_pathdir.listdir()
+
+    def delete(self, filename_list):
+        """Delete all files in filename list"""
+        assert type(filename_list) is not types.StringType
+        try:
+                for filename in filename_list:
+                        self.remote_pathdir.append(filename).delete()
+        except OSError, e:
+            raise BackendException(str(e))
+
