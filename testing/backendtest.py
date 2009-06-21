de8431e115ce9b38715557c5025340ff4f370a87
unknown <kenneth@loafman.com>
2009-06-21 12:54:22 -0500

    Make GIO tests dependent on presence of gio module.

--- testing/backendtest.py
+++ testing/backendtest.py
@@ -25,7 +25,11 @@ sys.path.insert(0, "../")
 
 import duplicity.backend
 import duplicity.backends
-import duplicity.backends.giobackend
+try:
+    import duplicity.backends.giobackend
+    gio_available = True
+except:
+    gio_available = False
 from duplicity.errors import *
 from duplicity import path, log, file_naming, dup_time, globals, gpg
 
@@ -250,35 +254,35 @@ class webdavsModuleTest(unittest.TestCase, UnivTest):
     password = config.webdavs_password
 
 
-class GIOTest(UnivTest):
-    """ Generic gio module backend class """
-    def setUp(self):
-        duplicity.backend.force_backend(duplicity.backends.giobackend.GIOBackend)
+if gio_available:
+    class GIOTest(UnivTest):
+        """ Generic gio module backend class """
+        def setUp(self):
+            duplicity.backend.force_backend(duplicity.backends.giobackend.GIOBackend)
 
-    def tearDown(self):
-        duplicity.backend.force_backend(None)
-
-
-class gioFileModuleTest(GIOTest, unittest.TestCase):
-    """ Test the gio file module backend """
-    my_test_id = "gio/file"
-    url_string = config.file_url
-    password = config.file_password
+        def tearDown(self):
+            duplicity.backend.force_backend(None)
 
 
-class gioSSHModuleTest(GIOTest, unittest.TestCase):
-    """ Test the gio ssh module backend """
-    my_test_id = "gio/ssh"
-    url_string = config.ssh_url
-    password = config.ssh_password
+    class gioFileModuleTest(GIOTest, unittest.TestCase):
+        """ Test the gio file module backend """
+        my_test_id = "gio/file"
+        url_string = config.file_url
+        password = config.file_password
 
 
-class gioFTPModuleTest(GIOTest, unittest.TestCase):
-    """ Test the gio ftp module backend """
-    my_test_id = "gio/ftp"
-    url_string = config.ftp_url
-    password = config.ftp_password
+    class gioSSHModuleTest(GIOTest, unittest.TestCase):
+        """ Test the gio ssh module backend """
+        my_test_id = "gio/ssh"
+        url_string = config.ssh_url
+        password = config.ssh_password
 
+
+    class gioFTPModuleTest(GIOTest, unittest.TestCase):
+        """ Test the gio ftp module backend """
+        my_test_id = "gio/ftp"
+        url_string = config.ftp_url
+        password = config.ftp_password
 
 if __name__ == "__main__":
     unittest.main()
