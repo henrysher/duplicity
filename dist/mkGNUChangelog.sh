0981295078e692093aad58bf23784270501dbe30
Kenneth Loafman <kenneth@loafman.com>
2009-07-01 12:21:38 -0500

    Changes for 0.6.01.

--- dist/mkGNUChangelog.sh
+++ dist/mkGNUChangelog.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# must be in root of CVS project
+# must be in root of BZR project
 cd /home/ken/workspace/duplicity-src
 
-# make changelog with full email addresses
-rcs2log -i 4 -l 79 \
--u $'loafman\tKenneth Loafman\tkenneth@loafman.com' \
--u $'bescoto\tBen Escoto\tben@emrose.org' \
--u $'jinty\tBrian Sutherland\tjinty@lentejasverdes.ath.cx' > Changelog.GNU
+# make changelog
+bzr log -v > Changelog.GNU
