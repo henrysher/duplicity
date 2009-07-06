eecb89fcd7ef735faa8a35c74c96e66e562c2224
Kenneth Loafman <kenneth@loafman.com>
2009-07-05 19:48:07 -0500

    Fix bug #394629 Hang on first collection-status

--- duplicity/dup_temp.py
+++ duplicity/dup_temp.py
@@ -175,9 +175,9 @@ class FileobjHooked:
         tgt = self.dirpath.append(self.remname)
         src_iter = SrcIter(src)
         if pr.compressed:
-            gpg.GzipWriteFile(src_iter, tgt.name)
+            gpg.GzipWriteFile(src_iter, tgt.name, size = sys.maxint)
         elif pr.encrypted:
-            gpg.GPGWriteFile(src_iter, tgt.name, globals.gpg_profile)
+            gpg.GPGWriteFile(src_iter, tgt.name, globals.gpg_profile, size = sys.maxint)
         else:
             if src.name != tgt.name:
                 assert not os.system("ln -s %s %s" % (src.name, tgt.name))
@@ -194,7 +194,7 @@ class FileobjHooked:
         src_iter = SrcIter(src)
         pr = file_naming.parse(self.permname)
         if pr.compressed:
-            gpg.GzipWriteFile(src_iter, tgt.name)
+            gpg.GzipWriteFile(src_iter, tgt.name, size = sys.maxint)
             os.unlink(src.name)
         else:
             os.rename(src.name, tgt.name)
