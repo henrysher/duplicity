bff5d26568e670c79391704d6cd2693c35dcce2f
Larry Gilbert <larry+launchpad@l2g.to>
2009-09-20 21:32:37 -0700

    Use py.path to simplify a little

--- testing/_util.py
+++ testing/_util.py
@@ -21,10 +21,8 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 import config
-import os
+import os, py
 from subprocess import check_call
-
-DIR_STACK = []
 
 def extract_test_files():
     # Darwin (Mac OS X) has its own batch of test files
@@ -38,37 +36,8 @@ def extract_test_files():
     # raises subprocess.CalledProcessError if it fails
 
 def cleanup_test_files():
-    try:
-        os.unlink('temp2.tar')
-    except OSError:
-        pass
-    
-    remove_tree('testfiles')
-    remove_tree('tempdir')
-
-    return
-
-def pushdir(path):
-    cwd = os.getcwd()
-    os.chdir(path)
-    DIR_STACK.append(cwd)
-    return os.getcwd()
-
-def popdir():
-    os.chdir(DIR_STACK.pop())
-    return os.getcwd()
-
-def remove_tree(pathname):
-    # Use os.walk to traverse the tree. False = bottom-up.    
-    tree = os.walk( os.path.abspath(pathname), False )
-
-    for (remove_dir, dont_care, remove_files) in tree:
-        # Remove each file
-        for filename in remove_files:
-            os.unlink( os.path.join( remove_dir, filename ) )
-
-        # Remove the directory, and for each parent that is empty,
-        # remove those, too
-        os.rmdir(remove_dir)
-    
+    for path in ('temp2.tar', 'testfiles', 'tempdir'):
+        lpath = py.path.local(path)
+        if lpath.check():
+            lpath.remove(rec=1)
     return
