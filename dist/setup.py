1d146412dc9071d564dd34608535a9c9b42090fd
Kenneth Loafman <kenneth@loafman.com>
2010-10-06 09:37:22 -0500

    Upgrade setup dependency to Python 2.4 or later.

--- dist/setup.py
+++ dist/setup.py
@@ -26,7 +26,7 @@ from distutils.core import setup, Extension
 version_string = "$version"
 
 if sys.version_info[:2] < (2,3):
-    print "Sorry, duplicity requires version 2.3 or later of python"
+    print "Sorry, duplicity requires version 2.4 or later of python"
     sys.exit(1)
 
 incdir_list = libdir_list = None
