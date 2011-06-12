e28705f08e3bb0523ee3b037fb7f6a4c721ae0f2
Kenneth Loafman <kenneth@loafman.com>
2011-06-12 05:16:35 -0500

    512628     --exclude-filelist-stdin and gpg error with/without PASSPHRASE

--- duplicity/selection.py
+++ duplicity/selection.py
@@ -22,6 +22,7 @@
 import os #@UnusedImport
 import re #@UnusedImport
 import stat #@UnusedImport
+import sys
 
 from duplicity.path import * #@UnusedWildImport
 from duplicity import log #@Reimport
@@ -356,7 +357,7 @@ probably isn't what you meant.""") %
             tuple_list.append(tuple)
             if not tuple[1]:
                 something_excluded = 1
-        if filelist_fp.close():
+        if filelist_fp not in (sys.stdin,) and filelist_fp.close():
             log.Warn(_("Error closing filelist %s") % filelist_name)
         return (tuple_list, something_excluded)
 
