a39f72abb9fe2742cb94f55b21f3242532cba7a7
ken <Unknown>
2016-01-25 10:21:48 -0600

    - Fix submitter name in changelogs. - Change comment to be correct.

--- duplicity/backends/webdavbackend.py
+++ duplicity/backends/webdavbackend.py
@@ -113,11 +113,7 @@ class WebDAVBackend(duplicity.backend.Backend):
     """
 
     """
-    for better compatibility we send an empty listbody as described in
-    http://www.ietf.org/rfc/rfc4918.txt
-    "  A client may choose not to submit a request body.  An empty PROPFIND
-       request body MUST be treated as if it were an 'allprop' request.  "
-    it was retired because e.g. box.net didn't support <D:allprop/>
+    Request just the names.
     """
     listbody = '<?xml version="1.0"?><D:propfind xmlns:D="DAV:"><D:prop><D:resourcetype/></D:prop></D:propfind>'
 
