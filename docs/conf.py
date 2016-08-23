1ddd9486f11ca394aec9b2fb21c5e2e3b00230a8
ken <Unknown>
2016-08-23 11:28:17 -0500

    Fix build on RTD.

--- docs/conf.py
+++ docs/conf.py
@@ -425,3 +425,21 @@ epub_exclude_files = ['search.html']
 # If false, no index is generated.
 #
 # epub_use_index = True
+
+
+# -- Needed to build on RTD -----------------------------------------------
+
+import os
+import sys
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+
+MOCK_MODULES = ['librsync']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+if os.environ.get('READTHEDOCS') == 'True':
+    sys.path.insert(0, os.path.abspath('../.'))
