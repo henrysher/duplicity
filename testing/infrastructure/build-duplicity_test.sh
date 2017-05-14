e6e080cc60f654f4e79802ebaa0de0b128f9f3cc
DerNils <nils@tekampe.org>
2017-05-14 09:17:59 +0200

    added more test infrastructure

--- testing/infrastructure/build-duplicity_test.sh
+++ testing/infrastructure/build-duplicity_test.sh
@@ -2,5 +2,20 @@
 
 cd duplicity_test
 cp -p ../../../requirements.txt .
+cp -p ../id_rsa .
+cp -p ../id_rsa.pub .
+
 docker build --tag dernils/duplicitytest:latest .
 rm requirements.txt
+rm id_rsa
+rm id_rsa.pub 
+
+cd ..
+cd ftp_server
+docker build --tag dernils/duplicity_testinfrastructure_ftp:latest .
+
+cd ..
+cd ssh_server
+cp -p ../id_rsa.pub .
+docker build --tag dernils/duplicity_testinfrastructure_ssh:latest .
+rm id_rsa.pub 
