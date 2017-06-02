e7627da01b44915981314872c918bfd3e6c4be95
Kenneth Loafman <kenneth@loafman.com>
2017-06-02 09:34:37 -0500

    * Fix bug #1672540 with patch from Benoit Nadeau   - Rename would fail to move par files when moving across filesystems.   - Patch uses shutil.move() to do the rename instead.

--- duplicity/path.py
+++ duplicity/path.py
@@ -34,6 +34,7 @@ import socket
 import time
 import re
 import gzip
+import shutil
 
 from duplicity import tarfile
 from duplicity import file_naming
@@ -633,7 +634,7 @@ class Path(ROPath):
 
     def rename(self, new_path):
         """Rename file at current path to new_path."""
-        os.rename(self.name, new_path.name)
+        shutil.move(self.name, new_path.name)
         self.setdata()
         new_path.setdata()
 
